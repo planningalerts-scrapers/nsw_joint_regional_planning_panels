f39a365780ab868ea0dc8688b64641e151dc0ad4
Matthew Landauer <mlandauer@gmail.com>
2021-05-27 12:37:51 +1000

    Get detailed info for each application

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ ruby "2.5.8"
 
 gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
 gem "mechanize"
+gem "activesupport"
--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,6 @@
 require "mechanize"
+require "active_support/core_ext/string/filters"
+
 
 url = "https://www.planningportal.nsw.gov.au/planning-panel"
 
@@ -8,4 +10,23 @@ urls = page.at(".page__content .grid__row").element_children.map do |application
   (page.uri + application.at('a')["href"]).to_s
 end
 
-p urls
+urls.each do |url|
+  page = agent.get(url)
+  v = page.at(".project__details").search(".row").map do |row|
+    label = row.at("b").inner_text.squish
+    value = row.at("div").inner_text.squish
+    [label, value]
+  end
+  fields = Hash[v]
+
+  description = page.at(".field-field-project-description").inner_text.squish
+  record = {
+    "council_reference" => fields["DA number"],
+    "address" => fields["Project Address"] + ", NSW",
+    "description" => description,
+    "info_url" => url,
+    "date_scraped" => Date.today.to_s,
+    "date_received" => fields["Referral date"],
+  }
+  pp record
+end
